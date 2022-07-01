f2a2e8fcfa63084d6ba2463eea79dda980c94c5b
laecio22 <laecon22@hotmail.com>
2022-07-01 14:25:09 -0300

    Resolução  do  desafio  4  de  typescript

---
+++ dist/desafios/desafio3.js
@@ -0,0 +1,31 @@
+"use strict";
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+let total = 0;
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        total += soma;
+        campoSaldo.innerHTML = total.toString();
+    }
+    limparInput();
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        total = 0;
+        campoSaldo.innerHTML = "";
+    }
+}
+function limparInput() {
+    soma.value = '';
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        somarAoSaldo(Number(soma.value));
+    });
+}
+botaoLimpar.addEventListener('click', function () {
+    limparInput();
+    limparSaldo();
+});
